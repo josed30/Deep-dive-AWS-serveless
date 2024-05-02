af8e05ae64c237f8ec5aa48b4e05667d77ec782b
Jose_Martinez1 <jose_martinez1@epam.com>
2024-05-01 20:42:11 -0500

    fixes

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -10,9 +10,6 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
-import java.util.HashMap;
-import java.util.Map;
-
 
 @LambdaHandler(lambdaName = "hello_world",
 	roleName = "hello_world-role",
@@ -23,13 +20,33 @@ import java.util.Map;
 		authType = AuthType.NONE,
 		invokeMode = InvokeMode.BUFFERED
 )
-public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
+public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		System.out.println("Hello from lambda");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		resultMap.put("statusCode", 200);
-		resultMap.put("message", "Hello from Lambda");
-		return resultMap;
+	private static final int SC_OK = 200;
+	private static final int SC_BAD_REQUEST = 400;
+	private static final String PATH = "hello";
+	private static String RESPONSE = "{\"statusCode\": %d, \"message\": \"%s\"}";
+
+	private static String MESSAGE_OK = "Hello from Lambda";
+	private static String MESSAGE_BAD_REQUEST = "Bad request syntax or unsupported method. Request path: %s. HTTP method: %s";
+
+
+
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request, Context context) {
+		String method = request.getHttpMethod();
+		String path = request.getPath();
+
+		int code = SC_OK;
+		String message = MESSAGE_OK;
+
+		if(!PATH.equals(path)) {
+			code = SC_BAD_REQUEST;
+			message = String.format(MESSAGE_BAD_REQUEST, path, method);
+		}
+
+
+		return new APIGatewayProxyResponseEvent()
+				.withStatusCode(code)
+				.withBody(String.format(RESPONSE, code, message));
 	}
 }
