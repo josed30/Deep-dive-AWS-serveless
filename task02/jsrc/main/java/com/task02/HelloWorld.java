b0988a72984ce39ef554c6a1fee65b56c7e16d69
Jose_Martinez1 <jose_martinez1@epam.com>
2024-05-01 21:03:46 -0500

    fixes

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -9,7 +9,10 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import com.google.gson.Gson;
 
+import java.util.HashMap;
+import java.util.Map;
 
 @LambdaHandler(lambdaName = "hello_world",
 	roleName = "hello_world-role",
@@ -25,7 +28,7 @@ public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, A
 	private static final int SC_OK = 200;
 	private static final int SC_BAD_REQUEST = 400;
 	private static final String PATH = "hello";
-	private static String RESPONSE = "{\"statusCode\": %d, \"message\": \"%s\"}";
+	private static String RESPONSE = "{'statusCode': %d, 'message': '%s'}";
 
 	private static String MESSAGE_OK = "Hello from Lambda";
 	private static String MESSAGE_BAD_REQUEST = "Bad request syntax or unsupported method. Request path: %s. HTTP method: %s";
@@ -39,14 +42,18 @@ public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, A
 		int code = SC_OK;
 		String message = MESSAGE_OK;
 
-		if(!PATH.equals(path)) {
+		/*if(!PATH.equals(path)) {
 			code = SC_BAD_REQUEST;
 			message = String.format(MESSAGE_BAD_REQUEST, path, method);
-		}
+		}*/
+
+		Map<String, Object> resultMap = new HashMap<String, Object>();
+		resultMap.put("statusCode", SC_OK);
+		resultMap.put("message", MESSAGE_OK);
 
 
 		return new APIGatewayProxyResponseEvent()
 				.withStatusCode(code)
-				.withBody(String.format(RESPONSE, code, message));
+				.withBody(new Gson().toJson(resultMap));
 	}
 }
